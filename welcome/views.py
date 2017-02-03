3728d0a076c7e34295dd516a3d4464459471cf1a
iket0512 <iket.ag24@gmail.com>
2017-02-03 13:37:40 +0530

    welcome page

--- welcome/views.py
+++ welcome/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+import requests	
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+@csrf_exempt
+def welcome(request):
+	if(request.method=='GET'):
+		response_body={}
+		try:
+			response_body['success']=True
+			response_body['message']="Welcome data"
+			response_body['image_url']=request.scheme+'://'+request.get_host()+'/media/welcome/welcome.png'
+		except Exception,e:
+			response_body['success']=True
+			response_body['message']="Error"
+
+		return JsonResponse(response_body)
